eb24d65eed5c74694c05aaee0b2c44c60e1d9d8c
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-14 11:44:55 +0200

    switched to class component in Avatar + added star feat to give you FUCKING EPILEPSY !

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Avatar.css';
+
+class Avatar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      star: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className={this.state.star ? 'card star' : 'card'}>
+        <img
+          onClick={this.props.shout}
+          src={
+            this.props.image
+              ? this.props.image
+              : 'https://www.drupal.org/files/issues/default-avatar.png'
+          }
+          alt={this.props.firstName}
+        />
+        <div className='cardName'>
+          <h2>{this.props.firstName}</h2>
+          <h3>{this.props.lastName}</h3>
+        </div>
+        <div className='feats'>
+          <img
+            onClick={(e) => {
+              const newStarStatus = !this.state.star;
+              this.setState({
+                star: newStarStatus,
+              });
+            }}
+            className={!this.state.star && 'grayscale'}
+            src='https://vignette.wikia.nocookie.net/fantendo/images/4/46/NSMBU_Star.png/'
+            alt='Mario Star'
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Avatar;
