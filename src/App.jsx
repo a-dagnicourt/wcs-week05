72195a73a90b01006bab3db94255a5edb492925d
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-14 16:08:59 +0200

    switcehd App to class component and added display of workshops on buttons clicks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
 
 import Hello from './components/Hello.jsx';
+// import WorkshopButton from './components/WorkshopButton.jsx';
 import Counter from './components/Counter.jsx';
 import AvatarList from './components/AvatarList.jsx';
-import WorkshopButton from './components/WorkshopButton.jsx';
 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Hello />
-        <WorkshopButton />
-        <Counter />
-        <AvatarList />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpsons: false,
+      counter: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <Hello />
+
+          <div>
+            <button
+              className='waves-effect waves-light btn cyan'
+              onClick={(e) => {
+                this.setState({
+                  simpsons: true,
+                });
+              }}>
+              Simpson's Quotes
+            </button>
+            <button
+              className='waves-effect waves-light btn cyan'
+              onClick={(e) => {
+                this.setState({
+                  counter: true,
+                });
+              }}>
+              Counter
+            </button>
+          </div>
+          {this.state.simpsons && <AvatarList />}
+          {this.state.counter && <Counter />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
